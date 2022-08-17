6700c9fefba60958841a278e69ef434b72b0dd67
Your Name <you@example.com>
2022-08-16 22:35:26 -0600

    commented code

--- src/components/SongDisplay.vue
+++ src/components/SongDisplay.vue
@@ -1,33 +1,39 @@
 <template>
   <div>
-<section>
-</section>
+    <section class="picked_song">
+      <h1 ref="playing_song">pick a song</h1>
+    </section>
 
-    <section class="songs_container">
-      <article v-for="(song, index) in songs" :key="index">
-        <h3 @click="play_song">{{ song[`title`] }}</h3>
-      </article>
+    <section
+      class="songs_container"
+      v-for="(song, index) in songs"
+      :key="index"
+    >
+      <h2 class="song_class" @click="play_song">
+        {{ song[`title`] }}
+      </h2>
+      <h1 class="song_class">
+        {{ song[`artist`] }}
+      </h1>
+      <img :src="song[`img_url`]" />
     </section>
   </div>
 </template>
 
-<script>
-export default {
+<script>                           //^^ have a for loop that displays the songs [title][artist][img] on the page, when a song title is clicked it will call the function play_song ^^//
+export default {                  //vv Below when the play_song function is called it will inject text into the ref[playing_song](section) and it will change the text to be the name of the song that was clicked vv
+  methods: {
+    play_song(details) {
+      this.$refs[`playing_song`][`innerText`] = details[`target`][`innerText`];
+    },
+  },
 
-
-methods: {
-  play_song () {
-    
-  }
-},
-
-
-  data() {
+  data() {                                //vv An Array of Objects that contains keys and value's for the songs vv
     return {
       songs: [
         {
           title: `It aint me`,
-          artist: `Kygo, Selenga Gomez`,
+          artist: `Kygo, Selena Gomez`,
           song_id: 1,
           img_url: `https://upload.wikimedia.org/wikipedia/en/d/d3/It_Ain%27t_Me_%28Official_Single_Cover%29_by_Kygo_%26_Selena_Gomez.png`,
         },
@@ -49,5 +55,26 @@ methods: {
 };
 </script>
 
-<style scoped>
+<style scoped>  
+img {
+  width: 30%;
+  height: 80%;
+}
+
+.song_class {
+  margin-top: 20px;
+  margin-bottom: 20px;
+  display: grid;
+}
+
+.songs_container {
+  display: grid;
+  grid-auto-flow: row;
+  place-items: center;
+}
+
+.picked_song {
+  text-align: center;
+  margin-top: 30px;
+}
 </style>
